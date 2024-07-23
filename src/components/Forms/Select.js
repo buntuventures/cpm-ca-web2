696b1d585f3eb6d39694b5506e16ed8bf5864084
JamesAlaric <jamesalaric@gmail.com>
2024-07-23 07:11:03 +0100

    [FIX] reserv form adjust0

--- src/components/Forms/Select.js
+++ src/components/Forms/Select.js
@@ -1,11 +1,10 @@
 import React from "react";
 import classes from "./Forms.module.css";
-import ArrowDown from "/images/arrow_down-24px.svg";
 
 const Select = ({ styles, children, changed, name, value }) => (
   <select
     className={classes.SelectInput}
-    style={{ backgroundImage: `url(${ArrowDown})`, ...styles }}
+    style={{ backgroundImage: `url("/images/arrow_down-24px.svg")`, ...styles }}
     name={name}
     value={value}
     onChange={changed}
